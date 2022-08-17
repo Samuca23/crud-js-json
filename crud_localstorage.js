94369f90532395dad773328848b316ccc173cc4a
Samuca23 <samuelchiodini@hotmail.com>
2022-08-16 21:41:05 -0300

    prt1

--- crud_localstorage.js
+++ crud_localstorage.js
@@ -1,3 +1,19 @@
 class CrudLocalStorage {
-    
+
+    constructor() {
+        this.createLayoutCrud();
+    }
+
+    createLayoutCrud = () => {
+        
+    }
+
+    createDiv = (sClass = false, sId = false) => {
+        var oDivContainer = document.getElementsByClassName('container');
+
+        var newDiv = document.createElement('div');
+        newDiv.setAttribute('class', sClass);
+        newDiv.setAttribute('id', sId);
+    }
 }
+let teste = new CrudLocalStorage();
