e5e75cd5edcb76c6c62723945a5d81fe468ee76f
Samuca23 <samuelchiodini@hotmail.com>
2022-08-17 21:33:41 -0300

    prt2

--- crud_localstorage.js
+++ crud_localstorage.js
@@ -5,15 +5,50 @@ class CrudLocalStorage {
     }
 
     createLayoutCrud = () => {
-        
+        var oDivContainer = this.getDivContainer();
+        var oDivForm = this.createDiv('form');
+        var oInputName = this.createInput('Nome: ', 'name', 'name', 'text');
+
+        oDivContainer.appendChild(oDivForm);
+        oDivForm.appendChild(oInputName);
     }
 
     createDiv = (sClass = false, sId = false) => {
-        var oDivContainer = document.getElementsByClassName('container');
-
         var newDiv = document.createElement('div');
-        newDiv.setAttribute('class', sClass);
-        newDiv.setAttribute('id', sId);
+        sClass != false ? newDiv.setAttribute('class', sClass) : null;
+        sId != false ? newDiv.setAttribute('id', sId) : null;
+
+        return newDiv;
+    }
+
+    createInput = (sLabel, sClass = false, sId = false, sType = false) => {
+        var oLabel = this.createLabel(sLabel, sId);
+        var newInput = document.createElement('input');
+
+        sClass != false ? newInput.setAttribute('class', sClass) : null;
+        sId != false ? newInput.setAttribute('id', sId) : null;
+        sType != false ? newInput.setAttribute('type', sType) : null;
+
+        oLabel.innerText = sLabel;
+        oLabel.appendChild(newInput);
+
+        return oLabel;
+    }
+
+    createLabel = (sLabel, sFor) => {
+        var newLabel = document.createElement('label');
+        newLabel.setAttribute('id', sFor);
+        newLabel.innerText = sLabel;
+
+        return newLabel;
+    }
+
+    createSelect = () => {
+        
+    }
+
+    getDivContainer = () => {
+        return document.getElementById('container');
     }
 }
 let teste = new CrudLocalStorage();
